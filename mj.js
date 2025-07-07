5d33e501a7de6627ae28f2bec3b44342d09df409
Jovani-riko <yonask69@gmail.com>
2025-07-07 19:49:12 +0300

    add js update

--- mj.js
+++ mj.js
@@ -23,6 +23,7 @@ if(signupForm){
         const password = document.getElementById('password').value.trim();
         const confirmPassword = document.getElementById('confirm-password').value.trim();
         
+        let isValid = true;
 
         if(username === ''){
             displayError('usernameError', 'username is required');
@@ -33,10 +34,67 @@ if(signupForm){
             displayError('emailError', 'Email address is required.');
             isValid = false;
         } else if (!isValidEmail(email)){
-            displayError('emailError', 'please enter a valid email adress')
+            displayError('emailError', 'please enter a valid email adress');
+            isValid = false;
+
         }
+
+        if(password === ''){
+            displayError('passwordError', 'Password is Required.');
+            isValid = false;
+        
+        }
+
+        else if (password.length < 8 ){
+            displayError('passwordError', 'password must be at least 8 characters long.')
+            isValid = false;
+        }
+        
+
+        if(confirmPassword === ''){
+            displayError('passwordError', 'Confirm password is required.');
+            isValid = false;
+
+        }
+        else if(password !==confirmPassword){
+            displayError('passwordError', 'password must match!');
+            isValid = false;
+        }
+
+        if (isValid){
+            console.log('sign-up form submitted successfully!');
+            console.log('username:', username);
+            console.log('Email:', email);
+            console.log('Phone:', phone);
+//add sucess msg
+            console.log('Form is valid and ready to submit to a server!');
+        }
+
     
     
-    })
+    });
 }
 
+function displayError(elementId, message){
+    const errorElement = document.getElementById(elementId);
+    if(errorElement){
+        errorElement.textContent = message;
+    }
+}
+
+function clearErrors(){
+    const errorElement = document.querySelectorAll('.error-message');
+    errorElement.forEach(element => {
+        element.textContent = '';
+    });
+}
+
+
+function isValidEmail(email){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+
+
+console.log(signupForm);
